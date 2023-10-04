d3ea20d5017ab1892aea4112da23234b4423d4de
Nathan1188101 <njsecord@lakeheadu.ca>
2023-10-04 03:23:18 -0400

    updated the Game model with getter and setters. May need to add more to it. Need to continue adding these to the rest of my models.

--- NBA_Tracker/Models/Game.cs
+++ NBA_Tracker/Models/Game.cs
@@ -2,5 +2,25 @@
 {
     public class Game
     {
+        //The Primary Key
+        public int GameId { get; set; } // unique identifier for each game
+
+        //Date (maybe add start time as well?)
+        public DateTime Date { get; set; }  // represent the date when the game takes place 
+        //public TimeSpan StartTime { get; set; }
+
+        public string Location { get; set; } // here we are storing the city/area/location of where the game is taking place 
+
+        //Foreign keys 
+        public int HomeTeamID { get; set; } // these foreign keys link us to the Team model and the TeamID within to specify which teams are playing in said game
+        public int AwayTeamID { get; set; }
+
+        //parent ref
+        public Team HomeTeam { get; set; }  
+        public Team AwayTeam { get; set; } 
+
+
+
+
     }
 }
